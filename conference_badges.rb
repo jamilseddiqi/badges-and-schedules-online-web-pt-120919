18ee757d730e9f5d3b8dbade7c8cdce66470e2de
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-23 22:27:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,32 +1,42 @@
-def badge_maker(speaker)
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-puts "Hellow My name is #{speaker}"
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
 
+
+
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
+  return nuarray
+end
+
+# batch_badge_creator(people)
+
+
+
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return nuarray
+end
+
+# assign_rooms(people)
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
 end 
 
-
-# def batch_badge_creator(names)
-
-
- 
-#   end
-  
-# end
-
-# def batch_badge_creator(names)
-  
-#   end
-# end
-
-# def assign_rooms(names)
- 
- 
-#   end
-# end
-
-# def printer(speaker)
- 
-#   end
-  
-#   end
-# end
